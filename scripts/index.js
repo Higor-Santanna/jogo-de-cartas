772124a4343a58a0ce3275f3b5c669652edc62c1
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2025-02-19 17:23:16 -0300

    Retirada da carta coringa

--- scripts/index.js
+++ scripts/index.js
@@ -1,100 +1,43 @@
-import { drawJokerCard } from "./services/jokerCard.js";
-import { drawCardPlayer1, tirarCartaJogador2 } from "./services/cardsPlayers.js";
+import { tirarCartaJogador1, tirarCartaJogador2 } from "./services/cardsPlayers.js";
 
-const jokerButtonEvents = document.getElementById('tirar-carta-coringa')
+
 const buttonPlayer1 = document.getElementById('tirar-carta-jogador-1')
 const buttonPlayer2 = document.getElementById('tirar-carta-jogador-2')
-const takeCard = document.querySelectorAll('.botao-tirar-carta')
+//const takeCard = document.querySelectorAll('.botao-tirar-carta')
 
-jokerButtonEvents.addEventListener('click', () => {
-    drawJokerCard()
-    takeCard.forEach((botao) => {
-        botao.removeAttribute('disabled')
-        botao.classList.remove("disabled")
-        botao.classList.add("active")
-    })
-})
+buttonPlayer1.addEventListener('click', tirarCartaJogador1);
+buttonPlayer2.addEventListener('click', tirarCartaJogador2);
 
-buttonPlayer1.addEventListener('click', () => {
-    drawCardPlayer1()
-})
-
-buttonPlayer2.addEventListener('click', () => {
-    tirarCartaJogador2()
-})
-
-function nipeOfCard(card) {
-    const nipeCard = card.nipe
-    return nipeCard
+function discoverForceOfCard(value) {
+    const cardValues = {
+        "ACE": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+        "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+        "JACK": 11, "QUEEN": 12, "KING": 13
+    };
+    return cardValues[value] || 0;
 }
 
-function discoverForceOfCard(card) {
-    let value = card.value;
 
-    if (value === "ACE") {
-        value = 1;
-    } else if (value === "QUEEN") {
-        value = 11;
-    } else if (value === "JACK") {
-        value = 12;
-    } else if (value === "KING") {
-        value = 13;
-    } else {
-        value;
-    }
-
-    return value;
-}
-
-async function compareCards(jogador1, jogador2, coringa) {
-    setTimeout(function () {
-        if (jogador1.value && (jogador1.nipe === coringa.nipe) > jogador2.value) {
-            alertWinner('jogador 1')
-    
-        } else if (jogador1.value < jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertWinner('jogador 2')
-    
-        } else if (jogador1.value && (jogador1.nipe === coringa.nipe) > jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertWinner('jogador 1')
-    
-        } else if (jogador1.value && (jogador1.nipe === coringa.nipe) < jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertWinner('jogador 2')
-    
-        } else if (jogador1.value && (jogador1.nipe === coringa.nipe) < jogador2.value) {
-            alertWinner('jogador 1')
-    
-        } else if (jogador1.value > jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertWinner('jogador 2')
-    
-        } else if (jogador1.value && (jogador1.nipe === coringa.nipe) === jogador2.value) {
-            alertWinner('jogador 1')
-    
-        } else if (jogador1.value === jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertWinner('jogador 2')
-    
-        } else if (jogador1.value && (jogador1.nipe === coringa.nipe) === jogador2.value && (jogador2.nipe === coringa.nipe)) {
-            alertTie()
-    
-        } else if(jogador1.value > jogador2.value){
-            alertWinner('jogador 1')
-
-        } else if (jogador1.value < jogador2.value){
-            alertWinner('jogador 2')
-
-        } else if(jogador1.value === jogador2.value){
-            alertTie()
+function compareCards(jogador1, jogador2) {
+    setTimeout(() => {
+        if (jogador1.value > jogador2.value) {
+            alertWinner('Jogador 1');
+        } else if (jogador1.value < jogador2.value) {
+            alertWinner('Jogador 2');
+        } else {
+            alertTie();
         }
-    }, 4000)
+    }, 1000);
 }
 
 function alertWinner(jogador) {
-    alert(`${jogador} venceu`)
+    alert(`${jogador} venceu!`);
     window.location.reload();
 }
 
 function alertTie() {
-    alert(`Vish deu empate`)
+    alert("Empate!");
     window.location.reload();
 }
 
-export { nipeOfCard, discoverForceOfCard, compareCards }
+export { discoverForceOfCard, compareCards };
--- scripts/services/cardsPlayers.js
+++ scripts/services/cardsPlayers.js
@@ -1,43 +1,33 @@
-import { generateCard } from "./takeCard.js"
-import { cardCoringa, cardJogador1, cardJogador2 } from "../objects/cards.js"
-import { nipeOfCard, discoverForceOfCard, compareCards } from "../index.js"
+import { generateCard } from "./takeCard.js";
+import { compareCards, discoverForceOfCard } from "../index.js";
 
-let someonePlayed = false;
+const cardJogador1 = { suit: '', image: '', value: 0 };
+const cardJogador2 = { suit: '', image: '', value: 0 };
 
-async function drawCardPlayer1() {
-    const card = await generateCard()
-    cardJogador1.setCardJogador1(card)
-    const imageCard1 = cardJogador1.image
-    document.getElementById('carta-jogador-1').src = imageCard1
+async function tirarCartaJogador1() {
+    const card = await generateCard();
+    cardJogador1.suit = card.cards[0].suit;
+    cardJogador1.image = card.cards[0].image;
+    cardJogador1.value = discoverForceOfCard(card.cards[0].value); // Converte corretamente
 
-    nipeOfCard(cardJogador1)
-    discoverForceOfCard(cardJogador1)
+    document.getElementById('carta-jogador-1').src = cardJogador1.image;
 
-    if (someonePlayed) {
-        compareCards(cardJogador1, cardJogador2, cardCoringa);
-    } else {
-        someonePlayed = true;
-
-        return;
+    if (cardJogador2.value !== 0) {
+        compareCards(cardJogador1, cardJogador2);
     }
 }
 
 async function tirarCartaJogador2() {
-    const card = await generateCard()
-    cardJogador2.setCardJogador2(card)
-    const imageCard2 = cardJogador2.image
-    document.getElementById('carta-jogador-2').src = imageCard2
+    const card = await generateCard();
+    cardJogador2.suit = card.cards[0].suit;
+    cardJogador2.image = card.cards[0].image;
+    cardJogador2.value = discoverForceOfCard(card.cards[0].value); // Converte corretamente
 
-    nipeOfCard(cardJogador2)
-    discoverForceOfCard(cardJogador2)
+    document.getElementById('carta-jogador-2').src = cardJogador2.image;
 
-    if (someonePlayed) {
-        compareCards(cardJogador1, cardJogador2, cardCoringa);
-    } else {
-        someonePlayed = true;
-
-        return;
+    if (cardJogador1.value !== 0) {
+        compareCards(cardJogador1, cardJogador2);
     }
 }
 
-export { drawCardPlayer1, tirarCartaJogador2 }
+export { tirarCartaJogador1, tirarCartaJogador2 };
